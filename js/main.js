9e9c706fb8bedf92627b91ff90a83ceb81e8379a
podshivalov_aa <32939018+aapodshivalov@users.noreply.github.com>
2018-04-21 01:06:32 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,62 +1,43 @@
-
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture', false);
 
 function ucFirst(str) {
-  // только пустая строка в логическом контексте даст false
   if (!str) return str;
 
   return str[0].toUpperCase() + str.slice(1);
 }
 
-// var text;
-// var users = 0;
+
 xhr.onreadystatechange = function() {
   if (this.readyState != 4) return;
 
-  // по окончании запроса доступны:
-  // status, statusText
-  // responseText, responseXML (при content-type: text/xml)
-
   if (this.status != 200) {
-    // обработать ошибку
-    alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+    var errText = document.getElementById('err');
+	errText.innerHTML += 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался');
     return;
   }
 
-  // получить результат из this.responseText или this.responseXML
-
-  
-  var text = this.responseText;
-  var users = JSON.parse(text);
-  console.log(users);
+  func();
 }
 xhr.send();
 
 function func() {
-  var users = JSON.parse(xhr.responseText);
-  console.log(users);
-var reply_click = function()
+  users = JSON.parse(xhr.responseText);
+
+reply_click = function()
 {
-    // alert("Button clicked, id "+this.id+", text"+this.innerHTML);
-    ID=this.id;
-    console.log(ID);
-    console.log(users.results[ID].picture.large+'');
-    console.log(users.results[ID].location.street+'');
-    console.log(users.results[ID].location.city+'');
-    console.log(users.results[ID].location.state+'');
-    console.log(users.results[ID].email+'');
-    console.log(users.results[ID].phone+'');
-    var modal = document.getElementById('modal');
+    var ID=this.id;
+
+    modal = document.getElementById('modal');
 
 
-	var span = document.getElementsByClassName("close")[0];
+	span = document.getElementsByClassName("close")[0];
 
  
   	var imageL = document.createElement("img");               
 	imageL.src = users.results[ID].picture.large+'';  
-	var X = document.getElementById('X');  
+	X = document.getElementById('X');  
 	document.getElementById("X").appendChild(imageL);
 
 
@@ -84,72 +65,141 @@ var reply_click = function()
 
 
 	var email = document.createElement("P");
-	var emailText = document.createTextNode("email: " + users.results[ID].email + '');
+	var emailText = document.createTextNode("email: " + users.results[ID].email.toString());
 	email.appendChild(emailText);
 	document.getElementById("X").appendChild(email); 
 
 
 	var phone = document.createElement("P");
-	var phoneText = document.createTextNode("phone: " + users.results[ID].phone + '');
+	var phoneText = document.createTextNode("phone: " + (users.results[ID].phone).toString());
 	phone.appendChild(phoneText);
 	document.getElementById("X").appendChild(phone);
 
 
 
 modal.style.display = "block";
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
     modal.style.display = "none";
     X.innerHTML = '';
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
         X.innerHTML = '';
     }
 }
 }
-  for (var i = 0; i < 49; i++) {
+  for (var i = 0; i < users.results.length-1; i++) {
+  	var wrapListElement = document.createElement("div");
   	var image = document.createElement("img");                       
 	image.id = i+'';
 	var t = document.createTextNode(i+'');     
 	image.src = users.results[i].picture.medium+'';
-	image.appendChild(t);      
-	document.getElementById("list").appendChild(image);
+	image.appendChild(t); 
+	wrapListElement.appendChild(image);     
 
 
 	var name = document.createElement("P");
 	var nameText = document.createTextNode(ucFirst(users.results[i].name.title)+'. '+ucFirst(users.results[i].name.first)+' '+ucFirst(users.results[i].name.last));
 	name.appendChild(nameText);
-	document.getElementById("list").appendChild(name);    
+	wrapListElement.appendChild(name);   
 
-	var tagSelect = document.createElement("select"); 
-	tagSelect.id = 'select' + i;  
-	document.getElementById("list").appendChild(tagSelect); 
-	var tagOption1 = document.createElement("option");     
-	var tagOption2 = document.createElement("option");                        
-	var textOption1 = document.createTextNode('алфавитный порядок по первой букве');
-	var textOption2 = document.createTextNode('обратная сортировка');
-	tagOption1.appendChild(textOption1);
-	tagOption2.appendChild(textOption2);
-
-	document.getElementById('select' + i).appendChild(tagOption1);
-	document.getElementById('select' + i).appendChild(tagOption2);
+	
 
 	var tagBr1 = document.createElement("br");
 	var tagBr2 = document.createElement("hr");
-	document.getElementById("list").appendChild(tagBr1); 
-	document.getElementById("list").appendChild(tagBr2); 
+	wrapListElement.appendChild(tagBr1);
+	wrapListElement.appendChild(tagBr2);
+
+	document.getElementById("list").appendChild(wrapListElement); 
 	document.getElementById(i+'').onclick = reply_click;
 
-
-
 }
 }
 
-setTimeout(func, 1000);
 
 
+function sortABC() {
+	var dict = [];
+	var dict2 = [];
+	for (var i = 0; i < users.results.length-1; i++) {
 
+		var arr2 = users.results[i].name.last+' '+i+'';
+
+		dict2.push(arr2);
+		}
+
+	wrapper = document.getElementById("list");
+	wrapper.innerHTML = '';
+	for (var i = 0; i < users.results.length-1; i++) {
+
+		var j = (dict2).sort()[i].split(' ')[1];
+		var wrapListElement = document.createElement("div");
+	  	var image = document.createElement("img");                       
+		image.id = j+'';
+		var t = document.createTextNode(j+''); 
+		    console.log(j);
+		image.src = users.results[j].picture.medium+'';
+		image.appendChild(t); 
+		wrapListElement.appendChild(image);     
+
+
+		var name = document.createElement("P");
+		var nameText = document.createTextNode(ucFirst(users.results[j].name.title)+'. '+ucFirst(users.results[j].name.first)+' '+ucFirst(users.results[j].name.last));
+		name.appendChild(nameText);
+		wrapListElement.appendChild(name);  
+		
+
+		var tagBr1 = document.createElement("br");
+		var tagBr2 = document.createElement("hr");
+		wrapListElement.appendChild(tagBr1);
+		wrapListElement.appendChild(tagBr2);
+
+		document.getElementById("list").appendChild(wrapListElement); 
+		document.getElementById(j+'').onclick = reply_click;
+		};
+	};
+
+
+	function reverseABC() {
+	var dict = [];
+	var dict2 = [];
+	for (var i = 0; i < users.results.length-1; i++) {
+
+		var arr2 = users.results[i].name.last+' '+i+'';
+		dict2.push(arr2);
+		}
+
+
+	wrapper = document.getElementById("list");
+	wrapper.innerHTML = '';
+	for (var i = 0; i < users.results.length-1; i++) {
+
+		var j = (dict2).sort().reverse()[i].split(' ')[1];
+		var wrapListElement = document.createElement("div");
+	  	var image = document.createElement("img");                       
+		image.id = j+'';
+		var t = document.createTextNode(j+''); 
+		    console.log(j);
+		image.src = users.results[j].picture.medium+'';
+		image.appendChild(t); 
+		wrapListElement.appendChild(image);     
+
+
+		var name = document.createElement("P");
+		var nameText = document.createTextNode(ucFirst(users.results[j].name.title)+'. '+ucFirst(users.results[j].name.first)+' '+ucFirst(users.results[j].name.last));
+		name.appendChild(nameText);
+		wrapListElement.appendChild(name);      
+
+		
+
+		var tagBr1 = document.createElement("br");
+		var tagBr2 = document.createElement("hr");
+		wrapListElement.appendChild(tagBr1);
+		wrapListElement.appendChild(tagBr2);
+		document.getElementById("list").appendChild(wrapListElement); 
+		document.getElementById(j+'').onclick = reply_click;
+		};
+	};
